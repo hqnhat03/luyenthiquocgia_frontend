94be9bf83df03aa816bb36f5c6f0ba0d64c4ef60
hqnhat <nhatld1410@gmail.com>
2026-05-21 12:33:05 +0700

    fix: domain

--- src/components/login/SwitchLoginTab.tsx
+++ src/components/login/SwitchLoginTab.tsx
@@ -7,20 +7,20 @@ function SwitchLoginTab(){
     <TabsList className="grid w-full grid-cols-2 mb-8 h-12 p-1">
               <TabsTrigger value="student">
                 <Link
-                  href={`${process.env.NEXT_PUBLIC_DOMAIN}/login`}
+                  href={`https://${process.env.NEXT_PUBLIC_DOMAIN}/login`}
                   className="flex items-center gap-2 cursor-pointer h-full rounded-md data-[state=active]:bg-background data-[state=active]:shadow-sm text-xs sm:text-sm"
                 >
                   <User className="size-4" />
-                  Học viên
+                  Học sinh
                 </Link>
               </TabsTrigger>
               <TabsTrigger value="teacher">
                 <Link
-                  href={`teacher.${process.env.NEXT_PUBLIC_DOMAIN}/login`}
+                  href={`https://giaovien.${process.env.NEXT_PUBLIC_DOMAIN}/login`}
                   className="flex items-center gap-2 cursor-pointer h-full rounded-md data-[state=active]:bg-background data-[state=active]:shadow-sm text-xs sm:text-sm"
                 >
                   <GraduationCap className="size-4" />
-                  Giảng viên
+                  Giáo viên
                 </Link>
               </TabsTrigger>
             </TabsList>
