301ca410ddecc3b22be885f26d13ddd8ab7a38ab
hqnhat <nhatld1410@gmail.com>
2026-05-21 12:38:06 +0700

    fix: domain

--- src/components/login/SwitchLoginTab.tsx
+++ src/components/login/SwitchLoginTab.tsx
@@ -1,3 +1,4 @@
+import { APP_LINKS } from '@/lib/links'
 import { GraduationCap, User } from 'lucide-react'
 import Link from 'next/link'
 import { TabsList, TabsTrigger } from '../ui/tabs'
@@ -7,7 +8,7 @@ function SwitchLoginTab(){
     <TabsList className="grid w-full grid-cols-2 mb-8 h-12 p-1">
               <TabsTrigger value="student">
                 <Link
-                  href={`https://${process.env.NEXT_PUBLIC_DOMAIN}/login`}
+                  href={`${APP_LINKS.student}/login`}
                   className="flex items-center gap-2 cursor-pointer h-full rounded-md data-[state=active]:bg-background data-[state=active]:shadow-sm text-xs sm:text-sm"
                 >
                   <User className="size-4" />
@@ -16,7 +17,7 @@ function SwitchLoginTab(){
               </TabsTrigger>
               <TabsTrigger value="teacher">
                 <Link
-                  href={`https://giaovien.${process.env.NEXT_PUBLIC_DOMAIN}/login`}
+                  href={`${APP_LINKS.teacher}/login`}
                   className="flex items-center gap-2 cursor-pointer h-full rounded-md data-[state=active]:bg-background data-[state=active]:shadow-sm text-xs sm:text-sm"
                 >
                   <GraduationCap className="size-4" />
